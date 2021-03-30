717b65c1f197e2056784192b161f975d10178f04
guix95 <guix95@163.com>
2021-03-30 09:38:12 +0800

    测试提交2

---
+++ com-sqltest/src/main/java/com/sqltest/entity/User.java
@@ -0,0 +1,12 @@
+package com.sqltest.entity;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author .abbeyleix
+ * @Date 2021/3/30 0030 上午 9:37
+ * @Version 1.0
+ **/
+public class User {
+
+}
